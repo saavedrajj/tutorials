d5d9fda81b93a625dc59f2878a99fec81ce65f49
Juan Saavedra <saavedrajj@gmail.com>
2016-07-12 21:47:39 +0100

    aaa

--- php/codigofacilito/video04/index.php
+++ php/codigofacilito/video04/index.php
@@ -7,26 +7,43 @@ class Loteria {
 	public $resultado = false;
 
 	// Métodos
-	public __construct ($numero, $intentos) {
-		$this->numero = $numero:
+	public function __construct ($numero, $intentos) {
+		$this->numero = $numero;
 		$this->intentos = $intentos;
 	}
 
 	public function sortear() {
-
+		$minimo = $this->numero / 2;
+		$maximo = $this->numero *2;
+		
+		for ($i=0; $i < $this->intentos;$i++) {
+			$int = rand($minimo , $maximo);
+			self::intentos($int);
+		}
 	}
 
-	public function intentos() {
-
+	public function intentos($int) {
+		if ($int == $this->numero) {
+			echo "<b>".$int ." == " . $this->numero . "</b><br>";
+			$this->resultado=true;
+		} else {
+			echo $int . " != " . $this->numero . "<br/>";
+		}
 	}
 
-	public __destruct {
 
+	public function __destruct() {
+		if ($this->resultado) {
+			echo "Felicidades, has acertado en " . $this->intentos . "intentos</br>";
+		}
+		else {
+			echo "Qué lastima, has perdido en " . $this->intentos . " intentos</br>";
+		}
 	}
 
 
 }
 
 $loteria = new Loteria(10,10);
-
+$loteria ->sortear();
 ?>
